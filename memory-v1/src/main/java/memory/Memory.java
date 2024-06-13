4940350098e94764e57ec0477e3ac72e8c63c02b
daeun <sss3598@gmail.com>
2024-06-13 20:40:15 +0900

    memory 조회 프로젝트 업로드

---
+++ memory-v1/src/main/java/memory/Memory.java
@@ -0,0 +1,14 @@
+package memory;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class Memory {
+
+  private long used;
+  private long max;
+}
