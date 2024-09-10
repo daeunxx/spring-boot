3a0946f5c35df71e13fc813ff65f2f02e516ed8b
daeun <sss3598@gmail.com>
2024-09-10 22:16:25 +0900

    스프링 | 의존 오브젝트 적용(DI)

--- src/main/java/org/example/helloboot/HelloController.java
+++ src/main/java/org/example/helloboot/HelloController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+  private final HelloService helloService;
+
+  public HelloController(HelloService helloService) {
+    this.helloService = helloService;
+  }
+
   public String hello(String name) {
-    SimpleHelloService helloService = new SimpleHelloService();
     return helloService.sayHello(Objects.requireNonNull(name));
   }
 }
---
+++ src/main/java/org/example/helloboot/HelloService.java
@@ -0,0 +1,6 @@
+package org.example.helloboot;
+
+public interface HelloService {
+
+  String sayHello(String name);
+}
