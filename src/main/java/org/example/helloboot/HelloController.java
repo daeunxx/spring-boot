8441e79bd404cffa28608593d16ea99e2969fcda
daeun <sss3598@gmail.com>
2024-09-10 22:47:19 +0900

    스프링 | Annotation Mapping

--- src/main/java/org/example/helloboot/HelloController.java
+++ src/main/java/org/example/helloboot/HelloController.java
@@ -1,9 +1,13 @@
 package org.example.helloboot;
 
 import java.util.Objects;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
+@RequestMapping("/hello")
 public class HelloController {
 
   private final HelloService helloService;
@@ -12,6 +16,9 @@ public class HelloController {
     this.helloService = helloService;
   }
 
+  //@RequestMapping(value = "/hello", method = RequestMethod.GET)
+  @GetMapping
+  @ResponseBody
   public String hello(String name) {
     return helloService.sayHello(Objects.requireNonNull(name));
   }
