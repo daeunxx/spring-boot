dda98919a229e91edf54040a4b6b8e00cd2da82d
daeun <sss3598@gmail.com>
2024-09-11 20:58:19 +0900

    스프링 | Bean 생명주기(ApplicationContext)

--- src/main/java/org/example/helloboot/HelloController.java
+++ src/main/java/org/example/helloboot/HelloController.java
@@ -1,6 +1,9 @@
 package org.example.helloboot;
 
 import java.util.Objects;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
   private final HelloService helloService;
+  private final ApplicationContext applicationContext;
 
-  public HelloController(HelloService helloService) {
+  public HelloController(HelloService helloService, ApplicationContext applicationContext) {
     this.helloService = helloService;
+    this.applicationContext = applicationContext;
+
+    System.out.println("applicationContext = " + applicationContext);
   }
 
   //@RequestMapping(value = "/hello", method = RequestMethod.GET)
