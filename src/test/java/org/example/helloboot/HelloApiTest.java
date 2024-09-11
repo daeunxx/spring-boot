a4f7c38d896656bdbe6455ce3635370e8109dce6
daeun <sss3598@gmail.com>
2024-09-11 21:20:42 +0900

    테스트 | Junit 기본 테스트

---
+++ src/test/java/org/example/helloboot/HelloApiTest.java
@@ -0,0 +1,31 @@
+package org.example.helloboot;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+
+  @Test
+  void hellApi() {
+    // http localhost:8080/hello?name=Spring
+    TestRestTemplate restTemplate = new TestRestTemplate();
+    ResponseEntity<String> response = restTemplate.getForEntity(
+        "http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+    // status code 200
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+    // header(content-type) text/plain
+    assertThat(response.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(
+        MediaType.TEXT_PLAIN_VALUE);
+
+    // body Hello Spring
+    assertThat(response.getBody()).isEqualTo("Hello Spring");
+  }
+}
