8b6f84fb11b0f5b00226820c197187b87c6841df
daeun <sss3598@gmail.com>
2024-09-13 16:55:45 +0900

    조건부 자동 구성 | Condition, @Conditional

--- src/main/java/org/example/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/org/example/config/autoconfig/TomcatWebServerConfig.java
@@ -1,15 +1,29 @@
 package org.example.config.autoconfig;
 
 import org.example.config.MyAutoConfiguration;
+import org.example.config.autoconfig.TomcatWebServerConfig.TomcatCondition;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(TomcatCondition.class)
 public class TomcatWebServerConfig {
 
   @Bean("tomcatWebServerFactory")
   public ServletWebServerFactory servletWebServerFactory() {
     return new TomcatServletWebServerFactory();
+  }
+
+  static class TomcatCondition implements Condition {
+
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+      return true;
+    }
   }
 }
