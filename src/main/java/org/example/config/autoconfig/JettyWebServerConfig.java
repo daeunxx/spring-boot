bd1e59394a81382b3dfc7e59b27ae46a3c3e8040
daeun <sss3598@gmail.com>
2024-09-13 21:35:49 +0900

    조건부 자동 구성 | 애노테이션 변수 활용 적용

--- src/main/java/org/example/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/org/example/config/autoconfig/JettyWebServerConfig.java
@@ -1,29 +1,17 @@
 package org.example.config.autoconfig;
 
+import org.example.config.ConditionalMyOnClass;
 import org.example.config.MyAutoConfiguration;
-import org.example.config.autoconfig.JettyWebServerConfig.JettyCondition;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
-@Conditional(JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
 
   @Bean("jettyWebServerFactory")
   public ServletWebServerFactory servletWebServerFactory() {
     return new JettyServletWebServerFactory();
-  }
-
-  static class JettyCondition implements Condition {
-
-    @Override
-    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-      return false;
-    }
   }
 }
