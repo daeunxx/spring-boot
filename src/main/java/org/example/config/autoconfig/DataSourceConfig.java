977fb227b4cb57e527a025917f2030844381ef76
daeun <sss3598@gmail.com>
2024-09-16 21:43:44 +0900

    Spring JDBC 자동 구성 | JdbcTemplate, JdbcTransactionManager

--- src/main/java/org/example/config/autoconfig/DataSourceConfig.java
+++ src/main/java/org/example/config/autoconfig/DataSourceConfig.java
@@ -7,12 +7,17 @@ import org.example.config.ConditionalMyOnClass;
 import org.example.config.EnableMyConfigProperties;
 import org.example.config.MyAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigProperties(MyDataSourceProperties.class)
+@EnableTransactionManagement
 public class DataSourceConfig {
 
   @Bean
@@ -38,5 +43,19 @@ public class DataSourceConfig {
     dataSource.setUsername(properties.getUsername());
     dataSource.setPassword(properties.getPassword());
     return dataSource;
+  }
+
+  @Bean
+  @ConditionalOnSingleCandidate(DataSource.class)
+  @ConditionalOnMissingBean
+  JdbcTemplate jdbcTemplate(DataSource dataSource) {
+    return new JdbcTemplate(dataSource);
+  }
+
+  @Bean
+  @ConditionalOnSingleCandidate(DataSource.class)
+  @ConditionalOnMissingBean
+  JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+    return new JdbcTransactionManager(dataSource);
   }
 }
