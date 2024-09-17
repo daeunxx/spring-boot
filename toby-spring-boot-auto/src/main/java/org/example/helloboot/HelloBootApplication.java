e3796994db1b7360211f94ea8395aa39d60c9620
daeun <sss3598@gmail.com>
2024-09-17 16:11:57 +0900

    스프링 부트 자동 구성 | 자동 구성 결과 확인

--- toby-spring-boot-auto/src/main/java/org/example/helloboot/HelloBootApplication.java
+++ toby-spring-boot-auto/src/main/java/org/example/helloboot/HelloBootApplication.java
@@ -1,8 +1,11 @@
 package org.example.helloboot;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
@@ -18,6 +21,19 @@ public class HelloBootApplication {
   void init() {
     jdbcTemplate.execute(
         "create table if not exists hello(name varchar(50) primary key, count int)");
+  }
+
+  @Bean
+  ApplicationRunner run(ConditionEvaluationReport report) {
+    return args -> {
+      report.getConditionAndOutcomesBySource().entrySet().stream()
+          .filter(condition -> condition.getValue().isFullMatch())
+          .forEach(condition -> {
+            System.out.println(condition.getKey());
+            condition.getValue().forEach(c -> System.out.println("\t" + c.getOutcome()));
+            System.out.println();
+          });
+    };
   }
 
   public static void main(String[] args) {
