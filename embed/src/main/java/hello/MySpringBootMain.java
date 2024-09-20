f0126547ab49815104dfd83692415954240494ee
daeun <sss3598@gmail.com>
2024-09-20 21:52:37 +0900

    자동 구성 | 정렬 수정

--- embed/src/main/java/hello/MySpringBootMain.java
+++ embed/src/main/java/hello/MySpringBootMain.java
@@ -6,7 +6,7 @@ import hello.boot.MySpringBootApplication;
 @MySpringBootApplication
 public class MySpringBootMain {
 
-  public static void main(String[] args)  {
+  public static void main(String[] args) {
     System.out.println("MySpringBootMain.main");
     MySpringApplication.run(MySpringBootMain.class, args);
   }
